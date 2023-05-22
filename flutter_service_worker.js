0f0ea2beb0ea2db714584193dc62ff7d572dc872
Jonas Magalhães Silva <80786958+jonasmagalhaes-8@users.noreply.github.com>
2023-05-22 01:22:08 -0300

    Minor Update
    Corrige dropdowns antes de carregar do Banco informações e menores correções em nomeclaturas

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {
 "favicon.png": "4c93c334a66bc5553ecf6737c7e119e2",
 "icons/Icon-192.png": "830c034cc436bfbb2101809389039c04",
 "icons/Icon-512.png": "d44721455acbf409ac239cbf82bb8ea6",
-"index.html": "953688949545c97c7d76f3f65d8bb26e",
-"/": "953688949545c97c7d76f3f65d8bb26e",
-"main.dart.js": "ae9aaa260de3b8065074d4d0d3570262",
+"index.html": "29662e59a2295a054cbe72dd88bd84d3",
+"/": "29662e59a2295a054cbe72dd88bd84d3",
+"main.dart.js": "b9ac9c8dd44abc028eac26c41be08c4d",
 "manifest.json": "efb35f11c39213b4bdb60a856bb61848",
 "version.json": "90f1e6e6e2dfb6e2a2455954f33ed100"
 };
